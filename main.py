3f4ab0278cc326b34d5b82ebf0ac37f1f3254967
Ryan6666 <39811991+Ryan6666@users.noreply.github.com>
2021-12-22 18:00:05 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import dns.resolver
+import csv
+
+
+Outlook = 0
+Gmail = 0
+Others = 0
+NA = 0
+progress = 0
+limit = 5000000
+
+csv_reader = csv.reader(open("./top-1m.csv"))
+mxlist = []
+for line in csv_reader:
+    mxlist.append(line[1])
+    if len(mxlist) == limit:
+        break
+
+for web in mxlist:
+    dnslist = []
+    try:
+        answers = dns.resolver.resolve(web, 'MX')
+        for rdata in answers:
+            dnslist.append(str(rdata.exchange))
+    except dns.exception.Timeout as a:
+        print('dns.exception.Timeout:', a)
+        dnslist.append("NoAnswer")
+    except dns.resolver.NoAnswer as b:
+        print('dns.resolver.NoAnswer:', b)
+        dnslist.append("NoAnswer")
+    except dns.resolver.NXDOMAIN as c:
+        print('dns.resolver.NXDOMAIN:', c)
+        dnslist.append("NoAnswer")
+    except Exception as d:
+        print('UnkonwnError:', d)
+        dnslist.append("NoAnswer")
+
+    if "google" in dnslist[0]:
+        Gmail = Gmail + 1
+    elif "outlook" in dnslist[0]:
+        Outlook = Outlook + 1
+    elif "NoAnswer" in dnslist[0]:
+        NA = NA + 1
+    else:
+        Others = Others + 1
+    progress = progress + 1
+    print(Gmail, Outlook, Others, NA, "    ({}/{})".format(progress, limit), "{}%".format(round(progress/limit, 5)*100))
+print("Gmail: ", Gmail, "\nOutlook: ", Outlook, "\nOthers: ", Others, "\nN/A: ", NA)
